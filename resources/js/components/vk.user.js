bc36dd7c54c78ac35290e1489855c04204db87fb
DmirijBelitskij <dmitribelitskij@gmail.com>
2024-08-27 10:00:54 +0300

    Проверка группы по id, массив групп в local storage

--- resources/js/components/vk.user.js
+++ resources/js/components/vk.user.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
  
 
-     $('#get-groups').click(function() {
+    $('#get-groups').click(function() {
         $.ajax({
             url: getGroupsUrl,
             type: 'POST',
@@ -14,14 +14,25 @@ $(document).ready(function() {
             success: function(response) {
                 var groupsList = $('#groups-list');
                 groupsList.empty();
-
+    
                 // Проверяем, есть ли в ответе группы
                 if (response.response && response.response.items) {
+                    var groupsArray = [];
+    
                     $.each(response.response.items, function(index, group) {
-                        // Извлекаем и отображаем screen_name и id
+                        var groupData = {
+                            screen_name: group.screen_name,
+                            id: group.id
+                        };
+                        groupsArray.push(groupData);
+    
+                        // Отображаем группы на странице
                         var listItem = $('<li>').text('Screen name: ' + group.screen_name + ', ID: ' + group.id);
                         groupsList.append(listItem);
                     });
+    
+                    // Сохраняем массив групп в localStorage
+                    localStorage.setItem('vkGroups', JSON.stringify(groupsArray));
                 } else {
                     groupsList.html('<p>Группы не найдены.</p>');
                 }
@@ -32,12 +43,13 @@ $(document).ready(function() {
         });
     });
 
+
     $('#check-group').click(function() {
-        console.log(true);
-        
-        var groupId = $('#group_num').text().trim();
-            console.log(groupId);
-            
+        // Получаем ID группы из статичного элемента
+        var groupId = $('#group_num').text().trim().match(/(\d+)/)[0]; // Извлекаем только число
+        console.log('ID группы для проверки:', groupId);
+    
+        // Выполняем AJAX-запрос для получения групп
         $.ajax({
             url: getGroupsUrl,
             type: 'POST',
@@ -47,37 +59,35 @@ $(document).ready(function() {
                 v: '5.199',
                 extended: 1
             },
-            success: function(groupsResponse) {
-                if (groupsResponse && groupsResponse.response && groupsResponse.response.items) {
-                    var groups = groupsResponse.response.items;
+            success: function(response) {
+                var groupsArray = [];
+                var resultDiv = $('#group-check-result');
     
-                    // После получения групп, вызываем checkGroup для проверки наличия конкретной группы
-                    $.ajax({
-                        url: checkgroup,
-                        type: 'POST',
-                        data: {
-                            user_id: userId,
-                            group_id: groupId,
-                            groups: groups, // Передаем список групп в checkGroup
-                            access_token: 'vk1.a.3WgPlNrH6DgZwgYKwFVJA63Is_2kkcAmSL_Ng9Oh00uFPblO-n6LbF3xxVBOoIoL9c0yuhycoET-Ofz2959vbduOxv3cdi87KyhlV-AAKb4-CrJHQYaox2uDkxR949SonlOaI1sKiLisEL58P0zmUr3GrmI4JI5je3p_00OP-5U0hr6ZxjhFXAaRfGGMK7P1y0QLjplxYonC0gqhlbOLng',
-                            v: '5.199',
-                            extended: 1
-                        },
-                        success: function(response) {
-                            var resultDiv = $('#group-check-result');
-                            console.log('Response from checkGroup:', response); 
-                            if (response.exists) {
-                                resultDiv.html('<p>Группа с ID ' + groupId + ' существует в вашем списке групп.</p>');
-                            } else {
-                                resultDiv.html('<p>Группа с ID ' + groupId + ' не найдена в вашем списке групп.</p>');
-                            }
-                        },
-                        error: function(xhr, status, error) {
-                            $('#group-check-result').html('<p>Произошла ошибка: ' + error + '</p>');
-                        }
+                // Проверяем, есть ли в ответе группы
+                if (response.response && response.response.items) {
+                    $.each(response.response.items, function(index, group) {
+                        var groupData = {
+                            screen_name: group.screen_name,
+                            id: group.id
+                        };
+                        groupsArray.push(groupData);
                     });
+    
+                    // Сохраняем массив групп в localStorage
+                    localStorage.setItem('vkGroups', JSON.stringify(groupsArray));
+    
+                    // Проверяем, существует ли группа с заданным ID в списке
+                    var groupExists = groupsArray.some(function(group) {
+                        return group.id == groupId;
+                    });
+    
+                    if (groupExists) {
+                        resultDiv.html('<p>Группа с ID ' + groupId + ' существует в вашем списке групп.</p>');
+                    } else {
+                        resultDiv.html('<p>Группа с ID ' + groupId + ' не найдена в вашем списке групп.</p>');
+                    }
                 } else {
-                    $('#group-check-result').html('<p>Не удалось получить список групп.</p>');
+                    resultDiv.html('<p>Группы не найдены.</p>');
                 }
             },
             error: function(xhr, status, error) {
@@ -85,6 +95,61 @@ $(document).ready(function() {
             }
         });
     });
+    
+    // $('#check-group').click(function() {
+    //     console.log(true);
+        
+    //     var groupId = $('#group_num').text().trim();
+    //         console.log(groupId);
+            
+    //     $.ajax({
+    //         url: getGroupsUrl,
+    //         type: 'POST',
+    //         data: {
+    //             user_id: userId,
+    //             access_token: 'vk1.a.3WgPlNrH6DgZwgYKwFVJA63Is_2kkcAmSL_Ng9Oh00uFPblO-n6LbF3xxVBOoIoL9c0yuhycoET-Ofz2959vbduOxv3cdi87KyhlV-AAKb4-CrJHQYaox2uDkxR949SonlOaI1sKiLisEL58P0zmUr3GrmI4JI5je3p_00OP-5U0hr6ZxjhFXAaRfGGMK7P1y0QLjplxYonC0gqhlbOLng',
+    //             v: '5.199',
+    //             extended: 1
+    //         },
+    //         success: function(groupsResponse) {
+    //             if (groupsResponse && groupsResponse.response && groupsResponse.response.items) {
+    //                 var groups = groupsResponse.response.items;
+    
+    //                 // После получения групп, вызываем checkGroup для проверки наличия конкретной группы
+    //                 $.ajax({
+    //                     url: checkgroup,
+    //                     type: 'POST',
+    //                     data: {
+    //                         user_id: userId,
+    //                         group_id: groupId,
+    //                         groups: groups, // Передаем список групп в checkGroup
+    //                         access_token: 'vk1.a.3WgPlNrH6DgZwgYKwFVJA63Is_2kkcAmSL_Ng9Oh00uFPblO-n6LbF3xxVBOoIoL9c0yuhycoET-Ofz2959vbduOxv3cdi87KyhlV-AAKb4-CrJHQYaox2uDkxR949SonlOaI1sKiLisEL58P0zmUr3GrmI4JI5je3p_00OP-5U0hr6ZxjhFXAaRfGGMK7P1y0QLjplxYonC0gqhlbOLng',
+    //                         v: '5.199',
+    //                         extended: 1
+    //                     },
+    //                     success: function(response) {
+                            
+    //                         var resultDiv = $('#group-check-result');
+    //                         console.log('Response from checkGroup:', response); 
+    //                         if (response.exists) {
+    //                             resultDiv.html('<p>Группа с ID ' + groupId + ' существует в вашем списке групп.</p>');
+    //                         } else {
+    //                             resultDiv.html('<p>Группа с ID ' + groupId + ' не найдена в вашем списке групп.</p>');
+    //                         }
+    //                     },
+    //                     error: function(xhr, status, error) {
+    //                         $('#group-check-result').html('<p>Произошла ошибка: ' + error + '</p>');
+    //                     }
+    //                 });
+    //             } else {
+    //                 $('#group-check-result').html('<p>Не удалось получить список групп.</p>');
+    //             }
+    //         },
+    //         error: function(xhr, status, error) {
+    //             $('#group-check-result').html('<p>Произошла ошибка: ' + error + '</p>');
+    //         }
+    //     });
+    // });
 
 //     $('#check-group').click(function() {
 //     // Получаем ID группы из div
